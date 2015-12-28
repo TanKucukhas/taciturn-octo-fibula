4e16f371a58f9ac555990ccee68cfdd687ef343d
Besim Ersahin <besim@floki.com>
2015-12-28 18:51:56 +0200

    .NET proje start

---
+++ Kolify/Kolify/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kolify.MvcApplication" Language="C#" %>
---
+++ Kolify/Kolify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kolify.Startup))]
+namespace Kolify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
